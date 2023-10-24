f436779f2fd574896782154634532b042a348ddd
wnf <william@fluxrme.com>
2023-10-23 19:06:55 -0500

    completes lab without bonus

---
+++ routes/cats.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/fact', async function(req, res, next) {
+  const catFact = await fetch('https://catfact.ninja/fact')
+  .then(res => res.json())
+  console.log(catFact)
+  res.render('cat/fact', {catFact})
+});
+
+module.exports = router;
